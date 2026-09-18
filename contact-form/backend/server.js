1ea96926467e9ca16dcd704b30c4f8190473cad2
kajalkk1113 <kajalkk1113l@gmail.com>
2026-09-18 14:15:22 +0530

    change in script.js

--- contact-form/backend/server.js
+++ contact-form/backend/server.js
@@ -70,8 +70,9 @@ app.get("/contacts",async(req,res)=>{
     }
 })
 
-app.listen(3000,()=>{
-    console.log("server started");
-    
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`server started on port ${PORT}`);
+});
 
